c071a35d68bff4dc5289d3864269a5f4ee68910e
Ian Snyder-Sohl <spitball@Snyder-Sohl.hitronhub.home>
2023-02-18 19:30:13 -0600

    functioning todo list -- passess all mocha

---
+++ index.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#add-todo');
+const buttonEl = document.querySelector('button')
+const olListEl = document.querySelector('#todo-list')
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const input = form.querySelector('input[type="text"]');
+    if (input.value.trim() !== '') {
+        const newListItem = document.createElement('li')
+        const newButtonItem = document.createElement('button')
+        newListItem.appendChild(newButtonItem)
+        newButtonItem.textContent = input.value;
+        olListEl.appendChild(newListItem);
+        input.value = "";
+
+        let clickCount = 0;
+        newButtonItem.addEventListener('click', event => {
+            clickCount++
+            if (clickCount === 1) {
+                newButtonItem.style.textDecoration = 'line-through'
+            } else if (clickCount === 2) {
+                olListEl.removeChild(newListItem)
+            }
+        })
+    }
+});
